d81099ab35c21b813bb2036bd30b693c8a43f42f
KevinKaras <kevinkaras2@gmail.com>
2021-12-21 12:12:03 -0500

    finished sudoku solver

---
+++ COMPUTERS/OPERATING SYSTEMS/VS CODE/DSA CONCEPTS/RECURSION/PROBLEMS/Sudoku-Solver.js
@@ -0,0 +1,124 @@
+
+
+
+let board = [ 
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],       // x0 | y0 , y , y , y , y , y , y , y , y8
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],       // x
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],       // x
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],       // x
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],       // x
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],       // x
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],       // x                            // xGridInterval * 3 (2*3 = 6) -> board[xGridInterval].length   ||  
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],       // x                            // yGridInterval * 3 (2*3 = 6) -> board[yGridInterval * 3 + 3]
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],       // x8
+]
+
+
+
+
+
+function findNextEmpty(board){
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[i].length; j++){
+            // console.log(board[i][j])
+            if(board[i][j] === 0){
+                return {'x': i, 'y': j}
+            }
+        }
+    }
+    return false
+}
+
+
+
+//(0, 2) === 0 => 4
+function valid(x, y, board){
+    // CHECK ROW
+    let boardVal = board[x][y]
+    // console.log("Board Value: ", boardVal)
+    for(let i = 0; i < board[x].length; i++){
+        if(board[x][i] === boardVal && (i != y) && board[x][i] != 0){
+            return false
+        }
+    }
+
+
+    // CHECK COLUMN
+    // console.log("Board Value: ", boardVal)
+    // console.log("Board Coords: ", x, y)
+    for(let i = 0; i < board.length; i++){
+        if(board[i][y] === boardVal && (i != x) && board[i][y] != 0){
+            return false
+        }
+    }
+
+
+    // CHECK CLUSTER
+    // console.log("Board Value: ", boardVal)
+    // console.log("Board Coords: ", x, y)
+    xGridInterval = Math.floor(x/3)
+    yGridInterval = Math.floor(y/3)
+    // console.log(board[xGridInterval * 3][yGridInterval * 3], board[xGridInterval * 3][yGridInterval * 3])
+    
+    for(let i = xGridInterval * 3; i < ((xGridInterval * 3) + 3); i++){
+        for(let j = yGridInterval * 3; j < ((yGridInterval * 3) + 3); j++){
+            // console.log(board[i][j])
+            if(board[i][j] === boardVal && (i != x && j != y) && board[i][j] != 0){
+                return false
+            }
+        }
+        
+    }
+
+
+    return true;
+}
+
+function printBoard(board){
+    for(let i = 0; i < board.length; i++){
+        if(i % 3 === 0 && i != 0){
+            console.log("-----------------------")
+        }
+        let row = ""
+        for(let j = 0; j < board[i].length; j++){
+            if(j % 3 === 0 && j != 0){
+                row += " | "
+            }
+            if(j === 8){
+                row += board[i][j]
+            } else {
+                row += board[i][j] + " "
+            }
+        }
+        console.log(row)
+    }
+}
+let backtrack = 0
+function solve(x, y, board){
+    
+
+    for(let i = 1; i <= 9; i++){
+        // console.log("ENTERING ITERATION: ", board)
+        let emptyPos = findNextEmpty(board)   // {x: 0, y: 2}
+        if(emptyPos === false){
+            return true
+        }
+        board[emptyPos.x][emptyPos.y] = i
+        // console.log("AFTER SET: ", board)
+        if(valid(emptyPos.x, emptyPos.y, board)){
+            nextPos = findNextEmpty(board)
+            if(solve(nextPos.x, nextPos.y, board)){
+                return true
+            }
+        }
+        backtrack += 1
+        board[emptyPos.x][emptyPos.y] = 0
+    }
+}
+
+printBoard(board)
+// console.log(valid(0, 8))
+solve(0, 0, board)
+console.log("\n")
+printBoard(board)
+console.log("Back tracked " + backtrack + " many times")
