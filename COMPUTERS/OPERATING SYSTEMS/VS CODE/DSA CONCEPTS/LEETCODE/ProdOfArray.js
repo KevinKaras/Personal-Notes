116db88f62fce6c7d9c234fcf8d80f0a361a6e84
KevinKaras <kevinkaras2@gmail.com>
2022-05-01 12:12:45 -0400

    changed some examples of the code for later use

--- COMPUTERS/OPERATING SYSTEMS/VS CODE/DSA CONCEPTS/LEETCODE/ProdOfArray.js
+++ COMPUTERS/OPERATING SYSTEMS/VS CODE/DSA CONCEPTS/LEETCODE/ProdOfArray.js
@@ -1,24 +1,51 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- **/
- var productExceptSelf = function(nums) {
-    const res = [];
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+// **/
+
+
+//  var productExceptSelf = function(nums) {
+//     const res = [];
     
-    let product = 1;
+//     let product = 1;
     
-    for (let i = 0; i < nums.length; i++) {
-        res[i] = product;
-        product *= nums[i];
+//     for (let i = 0; i < nums.length; i++) {
+//         res[i] = product;
+//         product *= nums[i];
+//     }
+//     product = 1;
+//     for (let j = nums.length - 1; j >= 0; j--) {
+//         res[j] *= product;
+//         product *= nums[j];
+//     }
+    
+//     return res;
+    
+// };
+
+
+let arr = [1,2,3,4,5]
+function Adder(array){
+    let sum = 1;
+    let result = []
+
+    for(let i = 0; i < array.length;i++){
+        result[i] = sum
+        sum *= array[i]
+        
+        console.log(result)
     }
-    product = 1;
-    for (let j = nums.length - 1; j >= 0; j--) {
-        res[j] *= product;
-        product *= nums[j];
+    sum = 1
+    for(let i = array.length-1; i >= 0;i--){
+        result[i] *= sum
+        sum *= array[i]
+        
+        console.log(result)
     }
-    
-    return res;
-    
-};
+    return result
+}
+console.log(Adder(arr))
+
+
 
 
