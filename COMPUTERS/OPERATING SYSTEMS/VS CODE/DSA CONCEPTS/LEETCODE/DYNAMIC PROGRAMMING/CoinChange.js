e91569c974c339240f0bd2ed3e9189504c99a202
KevinKaras <kevinkaras2@gmail.com>
2022-05-18 12:12:45 -0400

    added coin change

---
+++ COMPUTERS/OPERATING SYSTEMS/VS CODE/DSA CONCEPTS/LEETCODE/DYNAMIC PROGRAMMING/CoinChange.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/coin-change/
+
+var coinChange = function(coins, amount) {
+  
+    let dp = Array(amount+1).fill(amount+1)
+    // [ 0, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12 ]
+    
+    dp[0] = 0
+    
+    for(let i = 1; i <= amount; i++){
+        for(let j = 0; j < coins.length; j++){
+            if(i - coins[j] >= 0){
+                dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1)
+            }
+        }
+    }
+    return dp[amount]==(amount+1) ? -1 : dp[amount]
+};
